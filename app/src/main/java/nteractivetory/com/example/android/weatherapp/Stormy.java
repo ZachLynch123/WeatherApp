a9c68cbfd1a48960b2b2383faaadc032b77b9a1d
Unknown <Zachary.Lynch1337@gmail.com>
2018-03-28 12:25:02 -0800

    Using the wrong way to make a network connection in order to learn what NOT to do

--- app/src/main/java/nteractivetory/com/example/android/weatherapp/Stormy.java
+++ app/src/main/java/nteractivetory/com/example/android/weatherapp/Stormy.java
@@ -2,12 +2,44 @@ package nteractivetory.com.example.android.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Stormy extends AppCompatActivity {
+
+    public static final String TAG = Stormy.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "2cd94b53ee1806c983d10d877b5c94bd";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" +
+                latitude + "," + longitude;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()){
+                Log.v(TAG, response.body().string());
+
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
+
+
     }
 }
