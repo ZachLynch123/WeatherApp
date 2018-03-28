18fac0518d546c47ffecb77c098ad4885c30811d
Unknown <Zachary.Lynch1337@gmail.com>
2018-03-28 12:03:09 -0800

    Initial Commit

---
+++ app/src/main/java/nteractivetory/com/example/android/weatherapp/Stormy.java
@@ -0,0 +1,13 @@
+package nteractivetory.com.example.android.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stormy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
