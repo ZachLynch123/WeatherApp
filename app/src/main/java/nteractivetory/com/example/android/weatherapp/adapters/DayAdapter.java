c97077d1dd58e0e2b80085bc9e231a3c3a50a5e5
Unknown <Zachary.Lynch1337@gmail.com>
2018-04-13 18:02:37 -0800

    Finished custom layout. Made new package for adapters and added one to populate the list view on the DailyForecast activity

---
+++ app/src/main/java/nteractivetory/com/example/android/weatherapp/adapters/DayAdapter.java
@@ -0,0 +1,35 @@
+package nteractivetory.com.example.android.weatherapp.adapters;
+
+
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class DayAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence[] getAutofillOptions() {
+        return new CharSequence[0];
+    }
+}
