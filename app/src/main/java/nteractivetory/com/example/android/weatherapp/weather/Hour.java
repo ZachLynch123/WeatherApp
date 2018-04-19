53601865ce39823c2b24703d0f1450d152f54058
Zach <zachary.lynch1337@gmail.com>
2018-04-19 15:29:32 -0800

    Made Hour class Parcelable by implimenting Parcelable to class and all of its methods

--- app/src/main/java/nteractivetory/com/example/android/weatherapp/weather/Hour.java
+++ app/src/main/java/nteractivetory/com/example/android/weatherapp/weather/Hour.java
@@ -1,11 +1,15 @@
 package nteractivetory.com.example.android.weatherapp.weather;
 
-public class Hour {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Hour implements Parcelable {
     private long mTime;
     private String mSummary;
     private double mTemperature;
     private String mIcon;
     private String mTimeZone;
+
 
     public long getTime() {
         return mTime;
@@ -45,5 +49,43 @@ public class Hour {
 
     public void setTimeZone(String timeZone) {
         mTimeZone = timeZone;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int i) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperature);
+        dest.writeString(mIcon);
+        dest.writeString(mTimeZone);
+    }
+    private Hour(Parcel in){
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperature = in.readDouble();
+        mIcon = in.readString();
+        mTimeZone = in.readString();
+
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>(){
+
+        @Override
+        public Hour createFromParcel(Parcel source) {
+            return new Hour(source);
+        }
+
+        @Override
+        public Hour[] newArray(int i) {
+            return new Hour[i];
+        }
+    };
+    public Hour(){
+
     }
 }
