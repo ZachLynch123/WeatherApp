7a1251ce2db1eeb72e121b28ee010fc950c70198
Zach <zachary.lynch1337@gmail.com>
2018-04-19 18:54:13 -0800

    Used Layout manager to hook up adapter to recyclerView in HourlyForecastActivity. Used Butterknife to bind recyclerview and eliminate some boiler plate code

--- app/src/main/java/nteractivetory/com/example/android/weatherapp/ui/HourlyForecastActivity.java
+++ app/src/main/java/nteractivetory/com/example/android/weatherapp/ui/HourlyForecastActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 
 import java.util.Arrays;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import nteractivetory.com.example.android.weatherapp.R;
 import nteractivetory.com.example.android.weatherapp.adapters.HourAdapter;
 import nteractivetory.com.example.android.weatherapp.ui.Stormy;
@@ -16,15 +21,25 @@ public class HourlyForecastActivity extends AppCompatActivity {
 
     private Hour[] mHours;
 
+    @BindView(R.id.recyclerVeiw) RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(Stormy.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
-        HourAdapter adapter = new HourAdapter(this, mHours);
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // if the recycler view has a fixed size, use this line. Helps keeps things fast
+        mRecyclerView.setHasFixedSize(true);
 
     }
 }
